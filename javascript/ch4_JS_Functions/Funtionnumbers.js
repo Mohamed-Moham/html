722d9eb61b774b82b30ac0847678225d2551b2bd
Mohamed Mohamed <mahan9334@gmail.com>
2026-07-03 11:13:19 +0100

    function

---
+++ javascript/ch4_JS_Functions/Funtionnumbers.js
@@ -0,0 +1,14 @@
+// function add() {
+//     var num1 = 12;
+//     var num2 = 20;
+    
+//     console.log(num1 + num2);
+ 
+// }
+// add();
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+add(12, 20);
---
+++ javascript/ch4_JS_Functions/script.js
@@ -0,0 +1,22 @@
+ function greet() {
+     console.log(" hello Mohamed");
+}
+
+ greet();
+
+function morning() {
+    console.log("Good Morning");
+}
+
+function afternoon() {
+    console.log("Good Afternoon");
+}
+
+function night() {
+    console.log("Good Night");
+}
+
+
+morning();
+afternoon();
+night();
